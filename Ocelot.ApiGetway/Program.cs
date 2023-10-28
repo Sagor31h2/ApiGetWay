0be4b72aecdf957954532bf1dec27bb954403c7f
sagor31h2 <sagor31h2@gmail.com>
2023-10-29 02:41:19 +0600

    configure ocelot dynamically

---
+++ Ocelot.ApiGetway/Program.cs
@@ -0,0 +1,37 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add ocelot
+#region == ocelot==
+var environment = builder.Environment;
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+                     .AddOcelot($"Configurations/Routes/{environment.EnvironmentName}", environment);
+builder.Services.AddOcelot();
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
+
+app.Run();
